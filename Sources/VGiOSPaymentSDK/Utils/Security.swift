e89ded7a885526eb2315351b0bf825900b643e5c
Waheed Afolabi <waheed.freelancer16@gmail.com>
2025-04-23 11:51:47 +0100

    add security scaffold to handle crytography as demanded by Flutterwave

--- Sources/VGiOSPaymentSDK/Utils/Security.swift
+++ Sources/VGiOSPaymentSDK/Utils/Security.swift
@@ -1,8 +1,16 @@
 //
-//  File.swift
-//  
+//  Security.swift
+//
 //
 //  Created by waheedCodes on 23/04/2025.
 //
 
 import Foundation
+
+class Security {
+    static func encrypt(data: String, using key: String) -> String? {
+        // Implement encryption according to Flutterwave's requirements
+        // This is a placeholder for the actual encryption implementation
+        return data
+    }
+}
